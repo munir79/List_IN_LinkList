cf9fa3534dc7c9b3d02ebdf6d737151180bc8f67
munir79 <jakirhossainmunir79@gmail.com>
2024-02-27 16:06:01 +0600

    insert at a any position

--- .vscode/LinkListModifierFunction.cpp
+++ .vscode/LinkListModifierFunction.cpp
@@ -23,4 +23,13 @@ myList.pop_front();
 for(int val :myList){
     cout<<val<<" ";
 }
+cout<<endl;
+
+//3. insert //Note if index is greater than size then this value will added cycle 
+myList.insert(next(myList.begin(),2),1060);
+for(int val : myList){
+    cout<<val<<" ";
+}
+cout<<endl;
+
 }
